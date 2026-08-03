31fb718c12eb459951b1382a5186c953d8904f6b
Chirag <chiragkanjariya0712@gmail.com>
2026-08-03 22:31:20 +0530

    status issue fuxed

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -284,7 +284,7 @@ class OrderController extends Controller
                     // Mark as ready to ship
                     $order->update([
                         'status' => Order::STATUS_READY_TO_SHIP,
-                        'shipping_partner_status' => Order::SHIPPING_PARTNER_READY_TO_SHIP,
+                        'shipping_partner_status' => Order::SHIPPING_PARTNER_SHIPMENT_CREATED,
                         'shipping_partner_error' => null
                     ]);
 
